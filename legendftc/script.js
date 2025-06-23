ee973831ef3d8d20fb1a95fc928044f2a538c04f
julia <cajuliawan@gmail.com>
2025-06-22 17:49:40 -0700

    Delete legendftc directory

--- legendftc/script.js
+++
@@ -1,7 +0,0 @@
-function toggleCard(card) {
-  card.classList.toggle('expanded');
-}
-
-function showContactInfo(info) {
-  document.getElementById('contact-info-box').innerText = info || "Click an icon to view contact details.";
-}
